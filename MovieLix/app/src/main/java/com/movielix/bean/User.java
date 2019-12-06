958f35e12b2bedc4baee26550f328c108efc131d
Daniel Mancebo <dani.mancebo_3@hotmail.com>
2019-12-06 23:06:30 +0100

    feat: added user bean

---
+++ MovieLix/app/src/main/java/com/movielix/bean/User.java
@@ -0,0 +1,62 @@
+package com.movielix.bean;
+
+import java.util.List;
+
+/**
+ * User class.
+ */
+
+public class User {
+
+    private int mId;
+
+    private String mEmail;
+    private String mName;
+
+    private List<User> mFriends;
+
+    private User() {}
+
+    public static class Builder {
+
+        private int id;
+
+        private String email;
+        private String name;
+
+        private List<User> friends;
+
+        public Builder(int id) {
+            this.id = id;
+        }
+
+        public Builder withEmail(String email) {
+            this.email = email;
+
+            return this;
+        }
+
+        public Builder named(String name) {
+            this.name = name;
+
+            return this;
+        }
+
+        public Builder withFriends(List<User> friends) {
+            this.friends = friends;
+
+            return this;
+        }
+
+        public User build() {
+            User user = new User();
+
+            user.mId = this.id;
+            user.mEmail = this.email;
+            user.mName = this.name;
+            user.mFriends = this.friends;
+
+            return user;
+        }
+    }
+}
