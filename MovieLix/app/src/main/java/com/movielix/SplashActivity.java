b0271151687a404013fa0a181210276489853188
Daniel Mancebo <dani.mancebo_3@hotmail.com>
2019-12-06 22:53:04 +0100

    feat: added splash screen

---
+++ MovieLix/app/src/main/java/com/movielix/SplashActivity.java
@@ -0,0 +1,14 @@
+package com.movielix;
+
+import android.os.Bundle;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+public class SplashActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(final Bundle savedInstanceState) {
+        setTheme(R.style.AppTheme);
+        super.onCreate(savedInstanceState);
+    }
+}
