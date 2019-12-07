7caa00d21bec1eb569b453b51aa8bac47b65d8fb
Daniel Mancebo <dani.mancebo_3@hotmail.com>
2019-12-07 20:04:17 +0100

    feat: app is now able to retrieve the FirebaseUser and connect to the analytics server

--- MovieLix/app/src/main/java/com/movielix/SplashActivity.java
+++ MovieLix/app/src/main/java/com/movielix/SplashActivity.java
@@ -2,26 +2,44 @@ package com.movielix;
 
 import android.content.Intent;
 import android.os.Bundle;
+import android.util.Log;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
+import com.movielix.constants.Constants;
+
 public class SplashActivity extends AppCompatActivity {
+
+    private FirebaseAuth mAuth;
 
     @Override
     protected void onCreate(final Bundle savedInstanceState) {
         setTheme(R.style.AppTheme);
         super.onCreate(savedInstanceState);
 
-        routeToAppropiateScreen(false);
+        // Initialize Firebase Auth
+        mAuth = FirebaseAuth.getInstance();
     }
 
-    private void routeToAppropiateScreen(boolean loggedIn) {
-        if (loggedIn){
+    @Override
+    public void onStart() {
+        super.onStart();
+
+        // Check if user is signed in (non-null) and update UI accordingly.
+        FirebaseUser currentUser = mAuth.getCurrentUser();
+        routeToAppropriateScreen(currentUser != null);
+    }
+
+    private void routeToAppropriateScreen(boolean signedIn) {
+        if (signedIn){
+            Log.d(Constants.TAG, "User is signed in");
 
         } else {
-            Intent intent = new Intent(this, IntroActivity.class);
+            Log.d(Constants.TAG, "User is NOT signed in");
 
-            startActivity(intent);
+            startActivity(new Intent(this, IntroActivity.class));
         }
 
         finish();
--- MovieLix/app/src/main/java/com/movielix/constants/Constants.java
+++ MovieLix/app/src/main/java/com/movielix/constants/Constants.java
@@ -2,5 +2,6 @@ package com.movielix.constants;
 
 public class Constants {
 
+    public static final String TAG     = "MOVIELIX";
     public static final String PACKAGE = "com.movielix";
 }
