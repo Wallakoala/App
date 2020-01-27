a6e96aa7c11749e15ba5605775a7abfad14456ed
Daniel Mancebo <dani.mancebo_3@hotmail.com>
2020-01-27 19:18:05 +0100

    fix: cleared activity stack after registering or logging in

--- MovieLix/app/src/main/java/com/movielix/login/LoginActivity.java
+++ MovieLix/app/src/main/java/com/movielix/login/LoginActivity.java
@@ -302,6 +302,7 @@ public class LoginActivity extends AppCompatActivity {
             {
                 // Move to the next screen
                 Intent intent = new Intent(LoginActivity.this, MainActivity.class);
+                intent.setFlags(Intent.FLAG_ACTIVITY_NEW_TASK | Intent.FLAG_ACTIVITY_CLEAR_TASK);
 
                 startActivity(intent);
 
--- MovieLix/app/src/main/java/com/movielix/login/RegisterActivity.java
+++ MovieLix/app/src/main/java/com/movielix/login/RegisterActivity.java
@@ -548,6 +548,7 @@ public class RegisterActivity extends AppCompatActivity {
             {
                 // Move to the next screen
                 Intent intent = new Intent(RegisterActivity.this, MainActivity.class);
+                intent.setFlags(Intent.FLAG_ACTIVITY_NEW_TASK | Intent.FLAG_ACTIVITY_CLEAR_TASK);
 
                 startActivity(intent);
 
