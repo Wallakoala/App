39f7aa22373da379f9f24f436c712c716ce89c1e
Daniel Mancebo <dani.mancebo_3@hotmail.com>
2020-02-20 20:43:59 +0100

    feat: log messages redirected to file

--- MovieLix/app/src/main/java/com/movielix/constants/Constants.java
+++ MovieLix/app/src/main/java/com/movielix/constants/Constants.java
@@ -4,4 +4,6 @@ public class Constants {
 
     public static final String TAG     = "MOVIELIX";
     public static final String PACKAGE = "com.movielix";
+
+    public static final String LOG_FILE = "movielix.log";
 }
